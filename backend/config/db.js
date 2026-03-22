aaf093367d1eefd29a9e7ea3c40633efb61e9628
Abhiii Jha <abhiijha9088@gmail.com>
2026-03-22 21:19:33 +0530

    Removed hardcoded MongoDB credentials

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,11 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-  await mongoose.connect('mongodb+srv://abhiijha9088_db_user:gcz9yu72h4PUMcPF@cluster0.bvjpmmo.mongodb.net/?appName=Cluster0')
-  .then(() => console.log('DB Connected'))
-}
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("DB Connected");
+  } catch (error) {
+    console.error("DB Connection Failed:", error.message);
+    process.exit(1);
+  }
+};
